2c4b6746df5445cce274cfd4174420048bca49b9
Armando <ja.gamez24@info.uas.edu.mx>
2026-04-06 10:15:16 -0700

    Limpia de enlaces

--- src/App.jsx
+++ src/App.jsx
@@ -23,38 +23,40 @@ function App() {
       .catch(() => setIsPlaying(false));
   };
 
-if (!started) {
-  return (
-    <div className="WelcomeScreen">
-      <audio ref={audioRef} src="/Hastamifinal.mp3" loop />
-      <div className="WelcomeContent">
-        <p className="subtitulo">Tenemos el honor de invitarte a nuestra boda</p>
-        <p className="nombres">Ramón & Liliana</p>
-        <div className="envelope-wrapper" onClick={handleStart}>
-          <div className="envelope">
-            <div className="envelope-flap"></div>
-            <div className="envelope-body">
-              <p className="envelope-text">💌</p>
+  if (!started) {
+    return (
+      <div className="WelcomeScreen">
+        <audio ref={audioRef} src="/Hastamifinal.mp3" loop />
+        <div className="WelcomeContent">
+          <p className="subtitulo">Tenemos el honor de invitarte a nuestra boda</p>
+          <p className="nombres">Ramón & Liliana</p>
+          <div className="envelope-wrapper" onClick={handleStart}>
+            <div className="envelope">
+              <div className="envelope-flap"></div>
+              <div className="envelope-body">
+                <p className="envelope-text">💌</p>
+              </div>
             </div>
+            <p className="tap-hint">Toca para abrir</p>
           </div>
-          <p className="tap-hint">Toca para abrir</p>
         </div>
       </div>
-    </div>
-  );
-}  return (
+    );
+  }
+
+  return (
     <>
       <div className='Parent'>
-        
+
         <audio ref={audioRef} src="/Hastamifinal.mp3" loop />
-        
+
         <div className="blur-blob"></div>
         <div className="blur-blob-2"></div>
-        
+
         <div className='DIVImagen'>
           <img className='Anillos' src="fondoooo.jpeg" alt="Anillos" />
         </div>
-        
+
         <div className='ContentCard'>
           <div className='Seccion'>
             <p>Te esperamos para celebrar nuestro amor</p>
@@ -62,22 +64,22 @@ if (!started) {
 
           <div className='BloquePadrinos'>
             <p className='NombrePadrino'>Ramón y Liliana</p>
-          </div>          
-          
-          <button 
-            className={`botonNombres ${isPlaying ? 'reproduciendo' : ''}`} 
+          </div>
+
+          <button
+            className={`botonNombres ${isPlaying ? 'reproduciendo' : ''}`}
             onClick={toggleMusic}
           >
             {isPlaying ? 'Pausar canción' : 'Escucha nuestra canción'}
           </button>
-          
+
           <div id="countdown" className="contador">
             <div><span id="days">00</span><small>Días</small></div>
             <div><span id="hours">00</span><small>Horas</small></div>
             <div><span id="minutes">00</span><small>Min</small></div>
             <div><span id="seconds">00</span><small>Seg</small></div>
           </div>
-          
+
           <div className='SeccionFamilia'>
             <div className='ContenedorPadres'>
               <div className='BloquePadres'>
@@ -112,7 +114,7 @@ if (!started) {
             <img className='CodigoQr' src="dressCode.png" alt="Vestido" />
             <p>Te esperamos con vestimenta formal</p>
           </div>
-          
+
           <div className='SeccionItinerario'>
             <p className='TituloRol' style={{ fontSize: '1rem', marginBottom: '20px' }}>Nuestro Itinerario</p>
             <div className='Timeline'>
@@ -134,7 +136,7 @@ if (!started) {
               </div>
             </div>
           </div>
-          
+
           <div className='ContenedorDireccion'>
             <button className='button' onClick={() => setShowMaps(!showMaps)}>
               <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" className="svg-icon">
@@ -150,12 +152,9 @@ if (!started) {
                 <a href="https://maps.app.goo.gl/1ceZTfJoWgFymBXp9?g_st=iw" target="_blank" className='SubBoton'>Misa</a>
                 <a href="https://maps.app.goo.gl/6hL9J18fZykK76Xz9?g_st=iw" target="_blank" className='SubBoton'>Jardín</a>
               </div>
-           <a href="https://maps.app.goo.gl/1ceZTfJoWgFymBXp9?g_st=iw" target="_blank" className='SubBoton'>Misa</a>
-                <a href="https://maps.app.goo.gl/6hL9J18fZykK76Xz9?g_st=iw" target="_blank" className='SubBoton'>Jardín</a>    </div>
- 92ebaac (Sobre de inicio y canción final)
             )}
           </div>
- 
+
           <a href="https://docs.google.com/forms/d/e/1FAIpQLSdEm4944jIyXny2czKgdQrZHHT54ydrQvvgQPEXZRj36rsn5g/viewform" target="_blank" className="button">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" className="svg-icon">
               <g strokeWidth="2" strokeLinecap="round" stroke="#fff">
